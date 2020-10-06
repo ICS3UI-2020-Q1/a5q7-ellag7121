a47a6cd980065071bb1896384fa3fbe6e3c06d12
ellag7121 <ellag7121@wrdsb.ca>
2020-10-06 15:25:34 +0000

    finished a5q7

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * ask user for number between 1 and 10, draw a box of * with that width and height
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a new scanner to get user input
+    Scanner input = new Scanner(System.in);
+
+    //initialize variables
+    int number; //the number given by the user
+
+    //ask user for number
+    System.out.println("Please enter a number between 1 and 10");
+    number = input.nextInt();
+
+    //make a for loop to draw the correct number of rows
+    for(int i = 1; i <= number; i++){
+      //make another for loop to draw each line of stars
+      for(int j = 1; j <= number; j++){
+        //draw a star if it is in the first or last columns
+        if((j == 1 || j == number) || (i == 1 || i == number)){ //if it is the first or last row, draw stars, or if it is in the first or last column, draw stars
+          System.out.print("*"); 
+        }else{
+          //if it is not in the first or last rows or columns, don't draw a star
+          System.out.print(" ");
+        }
+      }
+      //move down 1 line
+      System.out.println("");
+    }
   }
 }
